c35546e8c8bd294e4aee96762970c73d92a19d25
yusuf <yusufsaydullayev@gmail.com>
2021-01-01 15:59:50 +0500

    imgfix

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Image from './components/Web developer-1.jpg'
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="container-fluid">
-          <div className="row">
-            <div className="image">
-              <img src={Image} width="1100px" height="1600px" alt="" />
-            </div>
+        <div className="container">
+          <div className="row ">
+            <div className="image"></div>
           </div>
         </div>
       </div>
--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 ReactDOM.render(
   <React.StrictMode>
-    <link rel="stylesheet" type="text/css"
-      href="http://yui.yahooapis.com/2.7.0/build/reset-fonts-grids/reset-fonts-grids.css" media="all" />
-    <link rel="stylesheet" type="text/css" href="resume.css" media="all" />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
