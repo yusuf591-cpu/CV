8c72c821774eefd57d6e37bb6acd09cf08d79a12
yusuf <yusufsaydullayev@gmail.com>
2021-01-01 15:33:29 +0500

    images

--- src/App.js
+++ src/App.js
@@ -1,87 +1,15 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Image from './components/Web developer-1.jpg'
 class App extends Component {
   render() {
     return (
-      <div id="doc2" class="yui-t7">
-        <div id="inner">
-          <div id="hd">
-            <div class="yui-gc">
-              <div class="yui-u first">
-                <h1>Yusufboy Saydullayev</h1>
-                <h2>Web Developer</h2>
-              </div>
-              <div class="yui-u">
-                <div class="contact-info">
-                  <><a href="mailto:name@yourdomain.com">yusufsaydullayev@gmail.com</a></>
-                  <h3>(93) 591 01 26</h3>
-                  <h3> Tashkent, UZB</h3>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div id="bd">
-            <div id="yui-main">
-              <div class="yui-b">
-                <div class="yui-gf">
-                  <div class="yui-u first">
-                    <h2>Skills</h2>
-                  </div>
-                  <div class="yui-u">
-                    <div class="talent">
-                      <h2>Web Design</h2>
-                      <p>Assertively exploit wireless initiatives rather than synergistic core competencies. </p>
-                    </div>
-                    <div class="talent">
-                      <h2>Interface Design</h2>
-                      <p>Credibly streamline mission-critical value with multifunctional functionalities. </p>
-                    </div>
-                    <div class="talent">
-                      <h2>Project Direction</h2>
-                      <p>Proven ability to lead and manage a wide variety of design and development projects in team and
-                      independent situations.</p>
-                    </div>
-                  </div>
-                </div>
-                <div class="yui-gf">
-                  <div class="yui-u first">
-                    <h2>Technical</h2>
-                  </div>
-                  <div class="yui-u">
-                    <ul class="talent">
-                      <li>XHTML</li>
-                      <li>CSS</li>
-                      <li>Bootstrap 4,5</li>
-                      <li>Semantic</li>
-                      <li class="last">Javascript</li>
-                      <li class="last">ES5,6</li>
-                    </ul>
-                    <ul class="talent">
-                      <li>Nodejs</li>
-                      <li>PHP</li>
-                      <li class="last">React</li>
-                    </ul>
-                    <ul class="talent">
-                      <li>Github</li>
-                      <li>MongoDB</li>
-                      <li class="last">Linux</li>
-                    </ul>
-                  </div>
-                </div>
-                <div class="yui-gf">
-                  <div class="yui-u first">
-                    <h2>Experience</h2>
-                  </div>
-                  <div class="yui-u">
-                    <div class="job last">
-                      <h4>2001-2004</h4>
-                      <p>Globally re-engineer cross-media schemas through viral methods of empowerment. Proactively grow
-                      long-term high-impact human capital and highly efficient innovation. Intrinsicly iterate excellent
-                      e-tailers with timely e-markets.</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
+      <div className="App">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="image">
+              <img src={Image} width="1100px" height="1600px" alt="" />
             </div>
           </div>
         </div>
